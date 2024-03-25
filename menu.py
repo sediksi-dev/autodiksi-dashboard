80a77a1e8eaafe3d89cf6f172871ba91f2b68c4d
Muhamad Erza Wansyah <erzawansyah@gmail.com>
2024-03-25 15:17:05 +0700

    Refactor and add new menu

--- menu.py
+++ menu.py
@@ -1,16 +1,27 @@
 import streamlit as st
 
 
-def authenticated_menu():
-    st.sidebar.title("Menu")
-    st.sidebar.page_link("pages/dashboard.py", label="Dashboard")
+def bot_likrea_section():
     st.sidebar.divider()
-    st.sidebar.subheader("Bot Likrea")
+    st.sidebar.subheader("Artikel Bot")
     st.sidebar.page_link("pages/list_websites.py", label="Daftar Situs")
     st.sidebar.page_link("pages/add_website_main.py", label="Tambah Situs")
     st.sidebar.page_link("pages/article_queue.py", label="Antrian Artikel")
     st.sidebar.page_link("pages/error_processed.py", label="Artikel Gagal Diproses")
+
+
+def likrea_article_generator():
     st.sidebar.divider()
+    st.sidebar.subheader("Keyword Seeder")
+    st.sidebar.divider()
+
+
+def authenticated_menu():
+    st.sidebar.title("Menu")
+    st.sidebar.page_link("pages/dashboard.py", label="Dashboard")
+    bot_likrea_section()
+    likrea_article_generator()
+
     st.sidebar.button(
         "Logout",
         on_click=lambda: st.session_state.pop("auth"),
